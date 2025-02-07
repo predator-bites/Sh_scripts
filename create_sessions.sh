052746c0e396b9864a89aaf26a204a90987e4491
predator-bites <142865899+predator-bites@users.noreply.github.com>
2025-02-07 20:14:26 +0200

    Update create_sessions.sh

--- create_sessions.sh
+++ create_sessions.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Цветовые коды
+NEON_RED='\033[38;5;196m'
+NEON_BLUE='\033[38;5;45m'
+RESET='\033[0m'
+
+# Логотип
+logo() {
+    echo -e "
+${NEON_RED}  ____   ${NEON_BLUE}____  
+${NEON_RED} |  _ \\  ${NEON_BLUE}|  _ \\ 
+${NEON_RED} | | | | ${NEON_BLUE}| |_) |
+${NEON_RED} | |_| | ${NEON_BLUE}|  __/ 
+${NEON_RED} |____/  ${NEON_BLUE}|_|    
+${RESET}
+"
+}
+
+# Вызов логотипа
+logo
+echo -e "${NEON_BLUE}https://t.me/DropPredator${RESET}"
 # Prompt the user to input the number of sessions
 read -p "How many screen sessions do you want to start? " NUM_SESSIONS
 
